5fcc02562833cb968966c1620d04c2fef2ddbcfd
Edson Souza <edsonfrs@gmail.com>
2024-08-15 08:33:45 -0300

    Initial commit

---
+++ src/main/java/com/fullstackvox/GftJavaIa2024Application.java
@@ -0,0 +1,13 @@
+package com.fullstackvox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftJavaIa2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GftJavaIa2024Application.class, args);
+	}
+
+}
