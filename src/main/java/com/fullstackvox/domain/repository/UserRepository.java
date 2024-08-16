62d0e4f8c54dfd2e98f268fbcb01fab952735c9b
Edson Souza <edsonfrs@gmail.com>
2024-08-16 11:32:07 -0300

    Implementada a camada de domínio

---
+++ src/main/java/com/fullstackvox/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fullstackvox.domain.repository;
+
+import com.fullstackvox.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
